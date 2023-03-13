d0df1594e97fec5e4909eec88f6b3fc21fcf58c7
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-03-13 21:40:39 +0300

    firebase

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
