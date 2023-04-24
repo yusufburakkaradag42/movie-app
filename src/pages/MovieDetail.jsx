890edf6317ca3c37a323c98a70764cfc3c70c453
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-04-24 15:37:10 +0300

    end

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,98 +1,81 @@
-import React, { useContext, useState } from "react";
-import GoogleIcon from "../assets/icons/GoogleIcon";
-import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import VideoSection from "../components/VideoSection";
 
-const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { createUser, signUpProvider } = useContext(AuthContext);
+const MovieDetail = () => {
+  const [movieDetails, setMovieDetails] = useState("");
+  const [videoKey, setVideoKey] = useState();
+  const { id } = useParams();
+  const {
+    title,
+    poster_path,
+    overview,
+    vote_average,
+    release_date,
+    vote_count,
+  } = movieDetails;
 
-  //* birleştirilmiş state
-  // const [info, setInfo] = useState({
-  //   firstName: "",
-  //   lastName: "",
-  //   email: "",
-  //   password: "",
-  // });
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+  const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
+  const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+  const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const displayName = `${firstName} ${lastName}`;
-    createUser(email, password, displayName);
-  };
-
-  // const { email, password, firstName, lastName } = info;
-  // const hadleChange = (e) =>
-  //   setInfo({ ...info, [e.target.id]: e.target.value });
+  useEffect(() => {
+    axios
+      .get(movieDetailBaseUrl)
+      .then((res) => setMovieDetails(res.data))
+      .catch((err) => console.log(err));
+    axios
+      .get(videoUrl)
+      .then((res) => setVideoKey(res.data.results[0].key))
+      .catch((err) => console.log(err));
+  }, [movieDetailBaseUrl, videoUrl]);
 
   return (
-    <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
-      <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
-        <form onSubmit={handleSubmit}>
-          <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
-            Sign Up
-          </h2>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              type="text"
-              name="floating_text"
-              className="peer"
-              placeholder=" "
-              required
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <label htmlFor="floating_email">First Name</label>
+    <div className="md:container px-10 mx-auto py-5">
+      <h1 className="text-center text-white text-3xl">{title}</h1>
+      {videoKey && <VideoSection videoKey={videoKey} />}
+      <div className="md:container flex justify-center px-10">
+        <div className="flex flex-col lg:flex-row max-w-6xl rounded-lg bg-gray-100 shadow-lg">
+          <img
+            className=" lg:w-1/3 h-96 lg:h-[600px] object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+            src={poster_path ? baseImageUrl + poster_path : defaultImage}
+            alt="poster"
+          />
+          <div className="p-6 flex flex-col justify-between">
+            <div>
+              <h5 className="text-gray-900 text-xl font-medium mb-2">
+                Overview
+              </h5>
+              <p className="text-gray-700 text-base mb-4">{overview}</p>
+            </div>
+            <ul className="bg-gray-100 rounded-lg border border-gray-400 text-gray-900">
+              <li className="px-6 py-2 border-b border-gray-400 w-full rounded-t-lg">
+                {"Release Date : " + release_date}
+              </li>
+              <li className="px-6 py-2 border-b border-gray-400 w-full">
+                {"Rate : " + vote_average}
+              </li>
+              <li className="px-6 py-2 border-b border-gray-400 w-full">
+                {"Total Vote : " + vote_count}
+              </li>
+              <li className="px-6 py-2 border-gray-400 w-full rounded-t-lg">
+                <Link
+                  to={-1}
+                  className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+                >
+                  Go Back
+                </Link>
+              </li>
+            </ul>
           </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              name="floating_text"
-              type="text"
-              required
-              className="peer"
-              placeholder=" "
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <label htmlFor="floating_text">Last Name</label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              name="floating_email"
-              type="email"
-              className="peer"
-              placeholder=" "
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="floating_email">Email</label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              name="floating_password"
-              type="password"
-              className="peer"
-              placeholder=" "
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="floating_password">Password</label>
-          </div>
-          <button className="btn-danger" type="submit">
-            Register
-          </button>
-          <button
-            className="flex justify-between text-center btn-danger"
-            type="button"
-            onClick={() => signUpProvider()}
-          >
-            Continue with Google
-            <GoogleIcon color="currentColor" />
-          </button>
-        </form>
-       </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Register;
+export default MovieDetail;
